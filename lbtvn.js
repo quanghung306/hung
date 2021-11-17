acc3b86a17d6f7a8ebe11d13b0246bb5b14a2a45
quanghung306 <94045688+quanghung306@users.noreply.github.com>
2021-11-17 10:32:38 +0700

    Add files via upload

---
+++ lbtvn.js
@@ -0,0 +1,31 @@
+<html>
+
+
+let productList = [
+    {
+        name: "iphone 7",
+        quantity: 30
+    },
+    {
+        name: "Samsung S10",
+        quantity: 10
+    },
+    {
+        name: "Iphone X",
+        quantity: 5
+    },
+    {
+        name: "Xiaomi Redmi",
+        quantity: 1
+    },
+];
+        var arr=[]
+        for(let Sanpham of productList){
+        if(Sanpham.quantity<10){
+            console.log("  hang sap het han") ;
+            arr.push(Sanpham.quantity+Sanpham.name);
+            console.log(arr);
+        }
+    
+    }
+    </html>
